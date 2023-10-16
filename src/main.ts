c2be1b7ccf4601e549cf83ec1449e91bddfd03eb
S.MacLennan <sam@maclennan.dev>
2023-10-16 23:45:26 +1100

    Tailwind setup

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Particles from 'vue3-particles';
 
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(Particles)
 
 app.mount('#app')
